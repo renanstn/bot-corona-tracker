a3bebc05e07d2bd848bb4d5c951c2535517a105d
DocMcCoy <renan_ota@hotmail.com>
2020-03-18 17:41:24 -0300

    Adicionado passo de fechar conexão com o banco sempre

--- src/database/functions.py
+++ src/database/functions.py
@@ -1,4 +1,5 @@
 from .models import *
+from .db_conn import db
 
 def get_global_news():
     pass
@@ -8,12 +9,17 @@ def get_local_news():
 
 def add_user(chat_id):
     BotUser.add(chat_id)
+    db.close()
 
 def save_global_news(data):
     WorldData.add(data)
+    db.close()
 
 def save_local_news(data):
     BrazilData.add(data)
+    db.close()
 
 def get_users():
-    return [user.chat_id for user in BotUser.select()]
+    data = [user.chat_id for user in BotUser.select()]
+    db.close()
+    return data
