ee21bbb35c76388a828c91332fb00a6b1cc71770
Renan Santana <renan_ota@hotmail.com>
2020-03-19 00:22:57 -0300

    Alterado nome da variável de ambiente para o padrão Heroku

--- src/settings.py
+++ src/settings.py
@@ -7,6 +7,6 @@ load_dotenv()
 DATABASE = os.getenv('DATABASE'),
 USER = os.getenv('USER'),
 PASSWORD = os.getenv('PASS'),
-HOST = os.getenv('HOST'),
+HOST = os.getenv('DATABASE_URL'),
 PORT = os.getenv('PORT')
 TOKEN = os.getenv('TOKEN')
