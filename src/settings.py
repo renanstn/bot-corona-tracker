7c13a93d4ecabbb623ee6cc08902d46d47bf1116
Renan Santana <renan_ota@hotmail.com>
2020-03-19 00:58:55 -0300

    Ajustes

--- src/settings.py
+++ src/settings.py
@@ -1,12 +1,22 @@
 import os
+from urllib.parse import urlparse
 from dotenv import load_dotenv, find_dotenv
 
 
 load_dotenv(find_dotenv())
 
-DATABASE = os.getenv('DATABASE'),
-USER = os.getenv('USER'),
-PASSWORD = os.getenv('PASS'),
-HOST = os.getenv('DATABASE_URL'),
-PORT = os.getenv('PORT')
+url = urlparse(os.getenv('DATABASE_URL'))
+
+DATABASE = url.path
+USER = url.username
+PASSWORD = url.password
+HOST = url.hostname
+PORT = url.port
 TOKEN = os.getenv('TOKEN')
+
+# DATABASE = os.getenv('DATABASE'),
+# USER = os.getenv('USER'),
+# PASSWORD = os.getenv('PASS'),
+# HOST = os.getenv('DATABASE_URL'),
+# PORT = os.getenv('PORT')
+# TOKEN = os.getenv('TOKEN')
